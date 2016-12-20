53c6713d1e67c927555f4e6a51502d7b2032b9de
Luke <luke@neutec.com.tw>
2016-12-20 15:21:30 +0800

    add firebase api info

--- web/main.dart
+++ web/main.dart
@@ -3,10 +3,11 @@ import 'package:firebase_demo/application.dart';
 
 void main() {
   initializeApp(
-      apiKey: "TODO",
-      authDomain: "TODO",
-      databaseURL: "TODO",
-      storageBucket: "TODO");
+      apiKey: "AIzaSyCWrGH3LbJ3yV-G2LQVVaXKjTzxVGWWI74",
+      authDomain: "fir-demo-2b991.firebaseapp.com",
+      databaseURL: "https://fir-demo-2b991.firebaseio.com",
+      storageBucket: "fir-demo-2b991.appspot.com");
+      //  messagingSenderId: "671270477343"  -- firebase 3.6.1 ?
 
   new Application().setupItems();
 }
